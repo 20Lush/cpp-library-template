d62a1ee282f52f7d2758521c92cd29e0743603db
ZACHARY JOHNSON <zachfsx83@gmail.com>
2024-03-28 19:02:12 -0700

    [DevOps] Initial commit, repo setup

---
+++ executable/src/main.cpp
@@ -0,0 +1,15 @@
+
+#include "main.hpp"
+
+// Make sure to change the namespace EXECUTABLE to something more specific
+namespace lush::EXECUTABLE {
+
+    // Any forward declerations?  
+
+}
+
+int main(int argc, char** argv) {
+
+    std::cout << "Hello World!" << std::endl;
+
+}
